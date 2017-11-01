444af0b6bbd1ba7f78331376124b041a78657000
Nigel Bolton <RexLT@users.noreply.github.com>
2017-11-01 12:06:25 +1100

    Add files via upload

---
+++ CapsNumLocker/Form1.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ShowToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnHide = New System.Windows.Forms.Button()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.pbxStatus = New System.Windows.Forms.PictureBox()
+        Me.lblCapLockStatus = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.lblNumLockStatus = New System.Windows.Forms.Label()
+        Me.ContextMenuStrip1.SuspendLayout()
+        CType(Me.pbxStatus, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Text = "CapsNumLocker - Red is OFF/ Green is ON"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ShowToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(104, 48)
+        '
+        'ShowToolStripMenuItem
+        '
+        Me.ShowToolStripMenuItem.Name = "ShowToolStripMenuItem"
+        Me.ShowToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.ShowToolStripMenuItem.Text = "Show"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(93, 73)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 0
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnHide
+        '
+        Me.btnHide.Location = New System.Drawing.Point(12, 73)
+        Me.btnHide.Name = "btnHide"
+        Me.btnHide.Size = New System.Drawing.Size(75, 23)
+        Me.btnHide.TabIndex = 1
+        Me.btnHide.Text = "Hide"
+        Me.btnHide.UseVisualStyleBackColor = True
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList1.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'pbxStatus
+        '
+        Me.pbxStatus.Location = New System.Drawing.Point(12, 12)
+        Me.pbxStatus.Name = "pbxStatus"
+        Me.pbxStatus.Size = New System.Drawing.Size(49, 43)
+        Me.pbxStatus.TabIndex = 2
+        Me.pbxStatus.TabStop = False
+        '
+        'lblCapLockStatus
+        '
+        Me.lblCapLockStatus.AutoSize = True
+        Me.lblCapLockStatus.Location = New System.Drawing.Point(81, 16)
+        Me.lblCapLockStatus.Name = "lblCapLockStatus"
+        Me.lblCapLockStatus.Size = New System.Drawing.Size(87, 13)
+        Me.lblCapLockStatus.TabIndex = 3
+        Me.lblCapLockStatus.Text = "Caps Lock is ON"
+        Me.lblCapLockStatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Timer1
+        '
+        '
+        'Timer2
+        '
+        '
+        'lblNumLockStatus
+        '
+        Me.lblNumLockStatus.AutoSize = True
+        Me.lblNumLockStatus.Location = New System.Drawing.Point(83, 38)
+        Me.lblNumLockStatus.Name = "lblNumLockStatus"
+        Me.lblNumLockStatus.Size = New System.Drawing.Size(85, 13)
+        Me.lblNumLockStatus.TabIndex = 4
+        Me.lblNumLockStatus.Text = "Num Lock is ON"
+        Me.lblNumLockStatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(180, 113)
+        Me.Controls.Add(Me.lblNumLockStatus)
+        Me.Controls.Add(Me.lblCapLockStatus)
+        Me.Controls.Add(Me.pbxStatus)
+        Me.Controls.Add(Me.btnHide)
+        Me.Controls.Add(Me.btnExit)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Caps/Num Locker"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        CType(Me.pbxStatus, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnHide As System.Windows.Forms.Button
+    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents pbxStatus As System.Windows.Forms.PictureBox
+    Friend WithEvents lblCapLockStatus As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents ShowToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents lblNumLockStatus As System.Windows.Forms.Label
+
+End Class
---
+++ CapsNumLocker/Form1.vb
@@ -0,0 +1,136 @@
+﻿'------------------------------------------------------------------------------
+'  Caps Locker
+'------------------------------------------------------------------------------
+'  Written by Nigel Bolton, Bolton Business Services ABN 
+'  PO Box 74, Glenorchy TAS 7010
+'------------------------------------------------------------------------------
+'  This program shows an icon in the System Tray to indicate if the caps lock
+'  is on or off.
+'------------------------------------------------------------------------------
+'  This software is freeware.  It may be distrbuted as-is and no changes are 
+'  permitted.  All risk is assumed by the user if they use or install the software. 
+'------------------------------------------------------------------------------
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        '-- Find current caps lock status
+        UpdateCapsNumLockStatus()
+
+        '-- Setup timer for caps lock status check
+        Timer1.Interval = 100
+        Timer1.Start()
+
+        '-- Setup timer for hiding form immediately after opening
+        Timer2.Interval = 10
+        Timer2.Start()
+
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+
+        '-- Clean up icon
+        NotifyIcon1.Icon = Nothing
+        NotifyIcon1.Dispose()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnHide_Click(sender As Object, e As EventArgs) Handles btnHide.Click
+
+        Hide()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        '-- Check the caps lock status after the specified number of ticks
+        UpdateCapsNumLockStatus()
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+        '-- Stop timer & hide main form
+        Timer2.Stop()
+        Me.Hide()
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+
+        '-- Close app when exit if selected
+        Me.Close()
+
+    End Sub
+
+    Private Sub ShowToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ShowToolStripMenuItem.Click
+
+        '-- Show form if user requests
+        Me.Show()
+
+    End Sub
+
+    Private Sub UpdateCapsNumLockStatus()
+
+        '-- Check caps/number lock status
+        If My.Computer.Keyboard.CapsLock = True And My.Computer.Keyboard.NumLock = True Then
+
+            '-- Set labels on form
+            lblCapLockStatus.Text = "Caps Lock is ON"
+            lblNumLockStatus.Text = "Num Lock is ON"
+
+            '-- Update image on firm
+            pbxStatus.Image = My.Resources.imgCapsOnNumOn
+
+            '-- Update icon in system tray
+            NotifyIcon1.Icon = My.Resources.icoCapsOnNumOn
+        Else
+            If My.Computer.Keyboard.CapsLock = True And My.Computer.Keyboard.NumLock = False Then
+
+                '-- Set labels on form
+                lblCapLockStatus.Text = "Caps Lock is ON"
+                lblNumLockStatus.Text = "Num Lock is OFF"
+
+                '-- Update image on firm
+                pbxStatus.Image = My.Resources.imgCapsOnNumOff
+
+                '-- Update icon in system tray
+                NotifyIcon1.Icon = My.Resources.icoCapsOnNumOff
+            Else
+                If My.Computer.Keyboard.CapsLock = False And My.Computer.Keyboard.NumLock = True Then
+
+                    '-- Set labels on form
+                    lblCapLockStatus.Text = "Caps Lock is OFF"
+                    lblNumLockStatus.Text = "Num Lock is ON"
+
+                    '-- Update image on firm
+                    pbxStatus.Image = My.Resources.imgCapsOffNumOn
+
+                    '-- Update icon in system tray
+                    NotifyIcon1.Icon = My.Resources.icoCapsOffNumOn
+                Else
+
+                    '-- Set labels on form
+                    lblCapLockStatus.Text = "Caps Lock is OFF"
+                    lblNumLockStatus.Text = "Num Lock is OFF"
+
+                    '-- Update image on firm
+                    pbxStatus.Image = My.Resources.imgCapsOffNumOff
+
+                    '-- Update icon in system tray
+                    NotifyIcon1.Icon = My.Resources.icoCapsOffNumOff
+                End If
+            End If
+        End If
+        Refresh()
+
+    End Sub
+
+End Class
